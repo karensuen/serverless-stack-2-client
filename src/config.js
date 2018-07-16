87b48854d80c8fac5c3fa79f932bea04e87c2456
Karen Suen <karen@Nats-MacBook-Air.local>
2018-07-16 17:15:45 +0100

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,31 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-east-2",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-lagn0s4vfwby"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-east-2",
+    URL: "https://uwjscv4xwa.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_hhBPIOPkZ",
+    APP_CLIENT_ID: "662712ndogqnotgq02c5v7pmdr",
+    IDENTITY_POOL_ID: "us-east-2:8f1434c3-90e4-4f8d-a45e-8203b1ec9f2a"
   }
+};
+
+const prod = {
+  // provide details of production environment as above
+}
+
+//Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  //Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };

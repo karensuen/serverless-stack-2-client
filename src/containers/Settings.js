45d9f87a28784ea3c009492992a36fdc21f12dca
Karen Suen <karen@Nats-MacBook-Air.local>
2018-07-17 18:10:10 +0100

    Adding settings page

---
+++ src/containers/Settings.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { API } from "aws-amplify";
+
+export default class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  billUser(details) {
+    return API.post("notes", "/billing", {
+      body: details
+    });
+  }
+}
